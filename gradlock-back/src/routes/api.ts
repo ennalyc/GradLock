239a98ee9354738a44f32178ab48c3660d98afc2
ennalyc <ecsl2@cin.ufpe.br>
2025-07-15 09:29:30 -0300

    feat(reservation): add routes

--- gradlock-back/src/routes/api.ts
+++ gradlock-back/src/routes/api.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { RoomsController } from '../controllers/roomsController';
+import { ReservationsController } from 'src/controllers/reservationsController';
 
 const apiRouter = express.Router();
 
@@ -9,5 +10,11 @@ apiRouter.get('/rooms/:id', RoomsController.getRoomById);
 apiRouter.post('/rooms', RoomsController.createRoom);
 apiRouter.put('/rooms/:id', RoomsController.updateRoom);
 apiRouter.delete('/rooms/:id', RoomsController.deleteRoom);
+
+
+// Rotas de reserva
+apiRouter.post('/booking', ReservationsController.create)
+apiRouter.put('/booking/:id/attData', ReservationsController.updateDate)
+apiRouter.delete('/booking/:id/cancel', ReservationsController.cancel)
 
 export default apiRouter;
